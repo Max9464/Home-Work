36620e315b0bea88e3c5c9d7ee05dff55a3085ae
Max9464 <114645789+Max9464@users.noreply.github.com>
2022-10-14 13:38:19 +0300

    Delete Program.cs

--- Задание 2/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
-Console.WriteLine("Input first number ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input second Number ");
